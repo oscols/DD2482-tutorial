ac5ac44a35506d59e6a33ad7d57197ad6119e115
Oscar Olsson <oscols@kth.se>
2024-10-11 10:45:26 +0200

    Added files from start

--- ansible-disaster-recovery/assets/setup.sh
+++ ansible-disaster-recovery/assets/setup.sh
@@ -1,5 +1,6 @@
-mkdir -p /home/secret
+echo hej
+# mkdir -p /home/secret
 
-touch /home/secret/do_not_run_this_script
+# touch /home/secret/do_not_run_this_script
 
-chown -R $(whoami):$(whoami) /home/secret
+# chown -R $(whoami):$(whoami) /home/secret

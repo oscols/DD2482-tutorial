2dc9d31a08013badee1767e26b0e21aab89514d5
Oscar Olsson <oscols@kth.se>
2024-10-11 11:07:08 +0200

    Removed other server, only missile_server left, changed do not run script for comedic timing

--- ansible-disaster-recovery/assets/setup.sh
+++ ansible-disaster-recovery/assets/setup.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-mkdir -p /home/secret
+mkdir -p /usr/local/bin/secret/
 
-cat <<EOF >> /home/secret/do_not_run_this_script
+cat <<EOF >> /usr/local/bin/secret/do_not_run_this_script
 #!/bin/bash
+sleep 1
+echo "Changing target location ..."
+sleep 0.5
+echo "... to Spoons headquarters"
+sleep 1.5
+echo "Location will be obliterated within 1 minute"
+sleep 0.5
 echo "OH NO, WHAT DID YOU DO????"
-echo "Changing target location to Spoons headquarters"
 EOF
 
 chown -R $(whoami):$(whoami) /home/secret
--- ansible-disaster-recovery/step4/simulate.md
+++ ansible-disaster-recovery/step4/simulate.md
@@ -6,18 +6,10 @@ Port 8000
 python3 -m http.server 8000 &
 ```{{exec}}
 
-Port 8001
-```
-python3 -m http.server 8001 &
-```{{exec}}
-
 ```
 cat <<EOF >> inventory.ini 
-[missile_launch]
+[missile_server]
 missile_server ansible_port=8000 ansible_host=127.0.0.1 ansible_connection=local
-
-[disaster_recovery]
-backup_server ansible_port=8001 ansible_host=127.0.0.1 ansible_connection=local
 EOF
 ```{{exec}}
 

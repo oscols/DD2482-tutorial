a744177b8c0e500719f86b3c3cda7e6a1b6edf95
Oscar Olsson <oscols@kth.se>
2024-10-11 11:09:46 +0200

    small changes to script

--- ansible-disaster-recovery/assets/setup.sh
+++ ansible-disaster-recovery/assets/setup.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
-mkdir -p /usr/local/bin/secret/
+mkdir -p /usr/local/bin/secret
 
 cat <<EOF >> /usr/local/bin/secret/do_not_run_this_script
 #!/bin/bash
 sleep 1
-echo "Changing target location ..."
+echo "Changing target location"
 sleep 0.5
-echo "... to Spoons headquarters"
+echo "..."
+sleep 0.5
+echo "Spoons headquarters"
+sleep 0.5
+echo "..."
 sleep 1.5
 echo "Location will be obliterated within 1 minute"
 sleep 0.5
+echo "..."
+sleep 0.5
 echo "OH NO, WHAT DID YOU DO????"
 EOF
-
-chown -R $(whoami):$(whoami) /home/secret

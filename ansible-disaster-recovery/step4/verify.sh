c30a33dfeda245507893b0e0b33c28c5c3b7e21a
flug0 <hugo@u1.se>
2024-10-12 23:04:47 +0200

    hopefully fixed stupid bugs

--- ansible-disaster-recovery/step4/verify.sh
+++ ansible-disaster-recovery/step4/verify.sh
@@ -2,6 +2,6 @@ if ! nc -zv 127.0.0.1 8000 2>&1 | grep -q 'succeeded'; then
   exit 1
 fi
 
-if ! grep -q "[missile_server]" inventory.ini && grep -q "ansible_port=8000" inventory.ini; then
+if ! (grep -q "[missile_server]" inventory.ini && grep -q "ansible_port=8000" inventory.ini); then
   exit 1
 fi
--- ansible-disaster-recovery/step5/verify.sh
+++ ansible-disaster-recovery/step5/verify.sh
@@ -1,7 +1,7 @@
-if ! grep -q "Launching missile..." launch.yml && grep -q "dest: /usr/local/bin/launch_missile" launch.yml && grep -q "Under maintenance, please switch to the backup server." launch.yml && grep -q "dest: /usr/local/bin/abort" launch.yml; then
+if ! (grep -q "Launching missile..." launch.yml && grep -q "dest: /usr/local/bin/launch_missile" launch.yml && grep -q "Under maintenance, please switch to the backup server." launch.yml && grep -q "dest: /usr/local/bin/abort" launch.yml); then
   exit 1
 fi
 
-if ! grep -q "Missile launch aborted successfully!" recover.yml && grep -q "dest: /usr/local/bin/abort" recover.yml; then
+if ! (grep -q "Missile launch aborted successfully!" recover.yml && grep -q "dest: /usr/local/bin/abort" recover.yml); then
   exit 1
 fi

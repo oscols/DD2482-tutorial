66efdd8346de2f98176c78779fc3b86d61388425
flug0 <hugo@u1.se>
2024-10-12 23:08:53 +0200

    maybe fixed???

--- ansible-disaster-recovery/step5/verify.sh
+++ ansible-disaster-recovery/step5/verify.sh
@@ -1,3 +1,7 @@
+if ! ([ -f "launch.yml" ] && [ -f "recover.yml" ]); then
+  exit 1
+fi
+
 if ! (grep -q "Launching missile..." launch.yml && grep -q "dest: /usr/local/bin/launch_missile" launch.yml && grep -q "Under maintenance, please switch to the backup server." launch.yml && grep -q "dest: /usr/local/bin/abort" launch.yml); then
   exit 1
 fi

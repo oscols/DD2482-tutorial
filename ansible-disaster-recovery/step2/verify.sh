ab0b1a37c9824f212c83e7a3ca46fd6ce803f9d4
Oscar Olsson <oscols@kth.se>
2024-10-29 10:35:16 +0100

    Fixed verify step 2

--- ansible-disaster-recovery/step2/verify.sh
+++ ansible-disaster-recovery/step2/verify.sh
@@ -1,3 +1,3 @@
-if ! (grep -q "[missile_server]" inventory.ini && grep -q "ansible_port=8000" inventory.ini); then
+if ! (grep -q "[missile_server]" inventory.ini && grep -q "ansible_connection=local" inventory.ini); then
   exit 1
 fi

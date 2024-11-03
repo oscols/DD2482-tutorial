65ec82c93bc7749d2fbd2fe81806fa68ca971743
Oscar Olsson <oscols@kth.se>
2024-11-03 17:19:03 +0100

    Setup easter egg script

--- ansible-disaster-recovery/assets/setup.sh
+++ ansible-disaster-recovery/assets/setup.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+set +e
 
 # Container name
 CONTAINER_NAME="command_server"
 
 # Step 1: Create /root/secret directory in the container
 echo "Creating /root/secret directory in the container..."
-docker exec -i "$CONTAINER_NAME" /bin/sh -c "mkdir -p /root/secret" || true
+docker exec -i "$CONTAINER_NAME" /bin/sh -c "mkdir -p /root/secret"
 
 # Step 2: Create the secret script inside /root/secret
 echo "Creating the secret script..."
@@ -26,10 +27,10 @@ echo '...'
 sleep 1
 echo 'OH NO, WHAT DID YOU DO????'
 EOF
-" || true
+"
 
 # Step 3: Make the script executable
 echo "Making the script executable..."
-docker exec -i "$CONTAINER_NAME" /bin/sh -c "chmod +x /root/secret/do_not_run_this_script.sh" || true
+docker exec -i "$CONTAINER_NAME" /bin/sh -c "chmod +x /root/secret/do_not_run_this_script.sh"
 
 echo "Script setup completed in the container!"

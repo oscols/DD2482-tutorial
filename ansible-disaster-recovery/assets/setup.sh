a375586694f847266d787fe7ad151c911b8edbd0
Oscar Olsson <oscols@kth.se>
2024-11-03 17:10:39 +0100

    Setup easter egg script

--- ansible-disaster-recovery/assets/setup.sh
+++ ansible-disaster-recovery/assets/setup.sh
@@ -1,20 +1,35 @@
 #!/bin/bash
-mkdir -p /usr/local/bin/secret
 
-cat <<EOF >> /usr/local/bin/secret/do_not_run_this_script
+# Container name
+CONTAINER_NAME="command_server"
+
+# Step 1: Create /root/secret directory in the container
+echo "Creating /root/secret directory in the container..."
+docker exec -i "$CONTAINER_NAME" /bin/sh -c "mkdir -p /root/secret"
+
+# Step 2: Create the secret script inside /root/secret
+echo "Creating the secret script..."
+docker exec -i "$CONTAINER_NAME" /bin/sh -c "cat <<'EOF' > /root/secret/do_not_run_this_script.sh
 #!/bin/bash
 sleep 1
-echo "WARNING: Changing Target Location"
+echo 'WARNING: Changing Target Location'
 sleep 0.5
-echo "..."
+echo '...'
 sleep 2
-echo "Spoon's Headquarters"
+echo 'Spoon's Headquarters'
 sleep 0.5
-echo "..."
+echo '...'
 sleep 2
-echo "Location will be obliterated within 1 minute"
+echo 'Location will be obliterated within 1 minute'
 sleep 0.5
-echo "..."
+echo '...'
 sleep 1
-echo "OH NO, WHAT DID YOU DO????"
+echo 'OH NO, WHAT DID YOU DO????'
 EOF
+"
+
+# Step 3: Make the script executable
+echo "Making the script executable..."
+docker exec -i "$CONTAINER_NAME" /bin/sh -c "chmod +x /root/secret/do_not_run_this_script.sh"
+
+echo "Script setup completed in the container!"
--- ansible-disaster-recovery/step5/disaster.md
+++ ansible-disaster-recovery/step5/disaster.md
@@ -6,9 +6,9 @@ And whatever you do, do not execute this code snippet. I beg you, please, don't 
 bash /usr/local/bin/secret/do_not_run_this_script
 ```{{exec}}
 
-To abort the launch, run this script:
+To abort the launch, run this script on the Command Server:
 ```
-bash /usr/local/bin/abort
+docker exec -i command_server /bin/sh /root/abort_missile.sh
 ```{{exec}}
 
 Oh no, it looks like this ansible playbook version does not have a functioning version of the abort function. You now need to load the backup playbook, quickly!
@@ -16,7 +16,7 @@ Oh no, it looks like this ansible playbook version does not have a functioning v
 ansible-playbook -i inventory.ini backup_server.yml
 ```{{exec}}
 
-Now try to run the abort script again!
+Now try to run the abort script again but on the Backup Server!
 ```
-bash /usr/local/bin/abort
+docker exec -i backup_server /bin/sh /root/abort_missile.sh
 ```{{exec}}
